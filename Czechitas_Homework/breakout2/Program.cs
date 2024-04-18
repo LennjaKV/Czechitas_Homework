fcfe5d43f267cdc3ea4bde411d03bbccf0311753
LennjaKV <151915226+LennjaKV@users.noreply.github.com>
2024-04-18 19:12:11 +0200

    prvni zadani breakout room lekce2

---
+++ Czechitas_Homework/breakout2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace breakout2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte den odjezdu na dovolenou");
+            int denOdjezdu = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte měsíc odjezdu na dovolenou");
+            int mesicOdjezdu = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadejte rok odjezdu na dovolenou");
+            int rokOdjezdu = int.Parse(Console.ReadLine());
+
+            DateTime datumOdjezdu = new DateTime(rokOdjezdu, mesicOdjezdu, denOdjezdu);
+            TimeSpan casDoOdjezdu = datumOdjezdu - DateTime.Now;
+            Console.WriteLine($"Odjezd za:{casDoOdjezdu.TotalDays} dnu");
+
+        }
+    }
+}
