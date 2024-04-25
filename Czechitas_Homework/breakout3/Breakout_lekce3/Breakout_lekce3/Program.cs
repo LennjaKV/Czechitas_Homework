a9b409a7dd086b7fe98b0d4a0d92a51429a2feed
LennjaKV <151915226+LennjaKV@users.noreply.github.com>
2024-04-25 18:42:40 +0200

    novy breakout

---
+++ Czechitas_Homework/breakout3/Breakout_lekce3/Breakout_lekce3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Breakout_lekce3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
