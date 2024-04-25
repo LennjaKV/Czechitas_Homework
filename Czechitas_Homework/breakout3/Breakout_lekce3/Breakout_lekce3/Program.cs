38f1bd5292dd57e76cb6335d29f24cdd3f10b308
LennjaKV <151915226+LennjaKV@users.noreply.github.com>
2024-04-25 18:59:15 +0200

    prvni cast

--- Czechitas_Homework/breakout3/Breakout_lekce3/Breakout_lekce3/Program.cs
+++ Czechitas_Homework/breakout3/Breakout_lekce3/Breakout_lekce3/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ukol - Obratte poradi stringu
+            string palindrom = "Kuna nese nanuk";
+            char[] array = palindrom.ToCharArray();
+            Array.Reverse(array);
+            palindrom = new string(array);
+            Console.WriteLine($"Otáčím větu: {palindrom}");
+
+            //Ukol - Napiste funkci, ktera umi detekovat, ze se jedna o palindrom (zatim jen u slov) a pak z pole vypiste jen palindromy
+            string[] slova = new string[] { "kajak", "program", "rotor", "Czechitas", "madam", "Jarda", "radar", "nepotopen" };
+
+            //Ukol - opravte v tomto textu omylem zapnuty Caps Lock
+            string capsLock = "jAK mICROSOFT wORD POZNA ZAPNUTY cAPSLOCK";
+
+            //Ukol - rozsifrujte tuto zpravu - text byl zasifrovan tak, ze jsme kazde pismeno posunuli o jedno doprava: 'a' -> 'b'. 
+            string sifra = "Wzcpsob!qsbdf!.!hsbuvmvkj!b!ktfn!ob!Ufcf!qztoz";
         }
     }
 }
